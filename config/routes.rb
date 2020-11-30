00100438a180a16467a6cd6e48e5bce1b17497bf
lise pluger <lise@gmail.com>
2020-11-30 18:25:58 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users  
+  # resources :casts, except: [:edit, :update] do
+  #   resources :comments, only: [:create]
+  # end
+  resources :minicasts, except: [:edit, :update] 
+  resources :podcasts, only: [:show, :index] 
+
+  namespace :casts do
+    resources :comments, only: [:create], on: :member
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
