7eef35a10d7fa8b85de7ab3bafb86946877a6aa3
clycrs <carolinelaycuras@hotmail.fr>
2020-12-02 14:57:43 +0100

    finish seeds , miss audio minicasts

--- app/models/cast.rb
+++ app/models/cast.rb
@@ -1,12 +1,13 @@
 class Cast < ApplicationRecord
   belongs_to :user
-  has_many :cast_sub_categories
+  has_many :cast_sub_categories, dependent: :destroy
   has_many :sub_categories, through: :cast_sub_categories
   has_many :categories, through: :sub_categories
   has_one :portrait
 
   validates :format, inclusion: { in: %w[podcast minicast] }
   has_one_attached :photo
+  has_one_attached :audio
 
   validates :title, presence: true
   validates :description, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :casts
+  has_many :casts, dependent: :destroy
 
   has_one_attached :photo
 
