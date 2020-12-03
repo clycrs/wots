f1129e75bdb2f96811be99176dabc7e84ab6d81b
clycrs <carolinelaycuras@hotmail.fr>
2020-12-03 12:37:54 +0100

    work in progress

--- app/controllers/minicasts_controller.rb
+++ app/controllers/minicasts_controller.rb
@@ -8,4 +8,17 @@ class MinicastsController < ApplicationController
     @minicast = Cast.find(params[:id])
   end
 
+  def create
+    @minicast = Cast.new(cast_params)
+      @minicast.save
+    # redirect_to profile_path
+    # else
+    #   render "new"
+    # end
+  end
+
+  def cast_params
+    params.require(:cast).permit(:title, :description, :theme)
+  end
+
 end
