c938bd44dce6e2b8a035da4b9c819f1d460a05c0
lise pluger <lise@gmail.com>
2020-12-09 15:23:20 +0100

    comments without user

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :podcasts, only: [:show, :index]
 
   resources :casts, only: [:index] do
-    resources :comments, only: [:create], on: :member
+    resources :comments, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
