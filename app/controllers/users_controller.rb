986851b47ddf492ae25fb2d95441796948cb1778
clycrs <carolinelaycuras@hotmail.fr>
2020-12-10 12:46:32 +0100

    last link ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
   def show
-    
+
     @user = User.find(params[:id])
     @minicasts = @user.casts.where(format: "minicast")
   end
- 
-  
+
+
 
 end
